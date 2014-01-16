481ee3c2ad855acf0558c73869f1d1478c14c2ba
rubeus90 <rubeus90@gmail.com>
2014-01-16 15:19:30 +0100

    Trier dans le bon sens

--- TP-DIST.c
+++ TP-DIST.c
@@ -249,7 +249,7 @@ int tri_bulle(Info* tabInfo, int n) {
   int num_balayage, i;
   for (num_balayage = 1 ; num_balayage < n ; ++num_balayage) /* n-1 balayages */
     for (i = 1 ; i < n ; ++i) /* balayage n°i : n-1 comparaisons */
-      if ((tabInfo[i].estampille * 1000 + tabInfo[i].position) < (tabInfo[i-1].estampille * 1000 + tabInfo[i-1].position))  /* si deux elements ne sont pas dans l'ordre */
+      if ((tabInfo[i].estampille * 1000 + tabInfo[i].position) > (tabInfo[i-1].estampille * 1000 + tabInfo[i-1].position))  /* si deux elements ne sont pas dans l'ordre */
       permute(&tabInfo[i], &tabInfo[i-1]); /* alors on les echange */
   return 0;
 }
